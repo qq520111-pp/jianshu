fa1d1fb5f8104e7ba647e34566021ca6bd3ff6e9
hahaha <2443209156@qq.com>
2020-07-16 17:51:10 +0800

    '输入框动画修改'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { HashRouter, Route } from "react-router-dom";
 import store from "./store/index";
+import { Provider } from "react-redux";
 import routes from "./router/index";
+console.log(store.getState());
 
 class App extends React.Component {
     constructor(props) {
@@ -11,22 +13,24 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="contain" >
-                <HashRouter>
-                    {routes.map((item, index) => {
-                        var exact = item.exact;
-                        return (<Route
-                            exact={exact}
-                            path={item.path}
-                            key={index}
-                            render={(props) => {
-                                return <item.component></item.component>
-                            }}
-                        >
-                        </Route>)
-                    })}
-                </HashRouter>
-            </div>
+            <Provider store={store}>
+                <div className="contain" >
+                    <HashRouter>
+                        {routes.map((item, index) => {
+                            var exact = item.exact;
+                            return (<Route
+                                exact={exact}
+                                path={item.path}
+                                key={index}
+                                render={(props) => {
+                                    return <item.component></item.component>
+                                }}
+                            >
+                            </Route>)
+                        })}
+                    </HashRouter>
+                </div>
+            </Provider>
         );
     }
 }
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,12 +1,21 @@
 let obj = {
     search: {
         wValue: '',
-        value: ''
+        value: '123'
     }
 }
 
 function reducer(state = obj, action) {
-
+    if (action.type === 'change_input_width') {
+        var oldState = JSON.parse(JSON.stringify(state));
+        oldState.search.wValue = action.wValue;
+        return oldState
+    }
+    if (action.type === 'change_input_value') {
+        var oldState = JSON.parse(JSON.stringify(state));
+        oldState.search.value = action.value;
+        return oldState
+    }
     return obj
 }
 export default reducer
